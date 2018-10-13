f9824e8c8cf28b8771d7cf85b670fbe447d12562
selinaslan <selin.aslan7@gmail.com>
2018-10-13 21:17:32 +0300

    Yeni halini gönderiyorum.

---
+++ robofriend-react/src/CardList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => {
+    const cardsArray = robots.map((user,i )=> {
+return  <Card key={i} id={user.id} name={user.name} email={user.email}/>
+
+    })
+    return (
+        <div>
+           {cardsArray}
+        </div>
+
+    );
+
+}
+export default CardList;
